f098db3b2d162b650d63e7293f580f5faae9f729
Alex <53269726+alexporter7@users.noreply.github.com>
2025-09-16 08:10:07 -0500

    initial commit with fabric template

---
+++ src/client/java/com/github/alexporter7/MCToastClient.java
@@ -0,0 +1,10 @@
+package com.github.alexporter7;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCToastClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
